85e0555801cdbebd77f576b6a94c5ad91824bc46
ycroissant <ycroissant@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-07-18 18:16:54 +0000

    two new files r.squared and vuong added
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mhurdle@6 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/r.squared.R
@@ -0,0 +1,71 @@
+r.squared <- function(object,
+                      which = c("all", "zero", "positive"),
+                      type = c("regression", "mcfadden"),
+                      dfcor = FALSE,
+                      r2pos=c("rss","ess","cor")){
+
+  type <- match.arg(type)
+  which <- match.arg(which)
+  r2pos <- match.arg(r2pos)
+  
+  sel <- ifelse(!is.null(object$coef.names$sel), TRUE, FALSE)
+  reg <- ifelse(!is.null(object$coef.names$reg), TRUE, FALSE)
+  ifr <- ifelse(!is.null(object$coef.names$ifr), TRUE, FALSE)
+  
+  KX <- length(object$coef.names$reg)
+  KS <- length(object$coef.names$sel)
+  KP <- length(object$coef.names$ifr)
+  if (is.null(KS)) KS <- 0
+  if (is.null(KP)) KP <- 0
+
+  y <- model.response(model.frame(object))
+  n <- length(y)
+  no <- sum(y == 0)
+  po <- mean(y==0)
+  pp <- 1 - po
+
+  ## On attribute KS et KP et po * KX à l'explication des 0 et (1 - p)
+  ## * KX à celle des valeurs positives.
+
+  K <- switch(which,
+              positive = (1 - po) * KX,
+              zero     = KS + KP + po * KX,
+              all      = KS + KP + KX
+              )
+  Ko <- switch(which,
+               positive = (1 - po) * 2,
+               zero     = (KS > 0) + (KP > 0) + po * 2,
+               all      = (KS > 0) + (KP > 0) + 2
+               )
+  
+  if (type == "mcfadden"){
+    if (!dfcor) R2 <- 1 - logLik(object, which)/logLik(object, which, naive = TRUE)
+    else R2 <- 1 - (logLik(object, which)- K)/(logLik(object, which, naive = TRUE) - Ko)
+  }
+  if (type == "regression"){
+    if (which != "positive"){
+      pfo <- fitted(object, "zero")
+      eo <- 0 - pfo
+      if (po != 0){
+        if (!dfcor) R2n <- sum( (pfo - po)^2) / (no * po^2)
+        else R2n <- sum( (pfo - po)^2) / (no * po^2)
+      }
+      else R2n <- 0
+    }
+    if (which != "zero"){
+      yp <- y[y > 0]
+      yf <- fitted(object, "positive")[y > 0]
+      ym <- mean(yp)
+      if (r2pos=="ess") R2p <- sum( (yf - ym)^2 ) / sum( (yp - ym)^2)
+      if (r2pos=="rss") R2p <- 1 - sum( (yp - yf)^2 )/sum( (yp - ym)^2)
+      if (r2pos=="cor") R2p <- cor(yf, yp)^2
+    }
+    if (which == "all") R2 <- po * R2n + (1 - po) * R2p
+  }
+  result <- switch(which,
+                   all = R2,
+                   zero = R2n,
+                   positive = R2p
+                   )
+  result
+}
---
+++ pkg/R/vuong.R
@@ -0,0 +1,131 @@
+
+
+############################################################
+## les fonctions donnant les quantiles et les proba pour la
+## loi des Khi Deux pondérés
+############################################################
+
+pwchisq <- function(q, weights, lower.tail = TRUE, n = 1000){
+  K <- length(weights)
+  e <- matrix(rnorm(n*K)^2, n, K)
+  wcs <- apply(e, 1, function(x) sum(x*weights))
+  F <- ecdf(wcs)
+  ifelse(lower.tail, F(q), 1- F(q))
+}
+
+qwchisq <- function(p, weights, lower.tail = TRUE, n = 1000){
+  K <- length(weights)
+  e <- matrix(rnorm(n*K)^2, n, K)
+  wcs <- apply(e, 1, function(x) sum(x*weights))
+  ifelse(lower.tail, quantile(wcs, p), quantile(wcs, 1-p))
+}  
+
+##########################################################
+## Fonction Vuong 2 qui permet l'ensemble des tests 
+## pour modèles emboîtés, non emboîtés et partiellement
+## emboîtés, avec ou sans hypothèse vraie a priori
+##########################################################
+
+vuongtest <- function(x, y,
+                      type = c("non.nested", "nested", "overlapping"),
+                      hyp = FALSE){
+  type <- match.arg(type)
+  data.name <- c(
+                 paste(deparse(substitute(x))),
+                 paste(deparse(substitute(y)))
+                 )
+  set.seed(100)
+
+  ## for convenience, call x the larger model
+  ##   if (length(coef(x)) < length(coef(y))){
+  ##     oldy <- y
+  ##     y <- x
+  ##     x <- oldy
+  ##   }  
+  lx <- as.numeric(x$logLik)
+  ly <- as.numeric(y$logLik)
+  logLx <- sum(lx)
+  logLy <- sum(ly)
+  Kx <- length(coef(x))
+  Ky <- length(coef(y))
+  n <- length(lx)
+  
+  if (length(ly) != length(lx))
+    stop("the number of observations of the two models differ")
+  
+  LR <- logLx - logLy
+  w2 <- 1/n*sum((lx-ly)^2)-(1/n*LR)^2
+  
+  if (type == "non.nested"){
+    statistic <- c(z = LR/sqrt(n*w2))
+    alternative <- ifelse(statistic > 0,
+                          "The first model is better",
+                          "The second model is better")
+    method <- "Vuong Test (non-nested)"
+    pval <- pnorm(abs(statistic), lower.tail = FALSE)
+    parameter <- NULL
+  }
+  else{
+    gradx <- attr(x$logLik,"gradi")
+    grady <- attr(y$logLik,"gradi")
+    BF <- t(gradx)%*%gradx/n
+    BG <- t(grady)%*%grady/n
+    AF <- -solve(vcov(x))/n
+    AG <- -solve(vcov(y))/n
+  }
+  if (type == "nested"){
+    if (Kx == Ky) stop("the two models are not nested")
+    nestedOK <- prod(names(coef(y)) %in% names(coef(x)))
+    if (! nestedOK || (Kx == Ky)) stop("the two models are not nested")
+    parameter <- c(df = Kx - Ky)
+    statistic <- 2 * LR
+    method <- "Vuong Test (nested)"
+    alternative <- "The larger model is better"
+    if (! hyp){
+      names(statistic) <- "wchisq"
+      common.coef <- names(coef(x)) %in% names(coef(y))
+      trans <- diag(1,Kx)[common.coef, ]
+      W <- BF%*%(t(trans)%*%solve(AG)%*%trans-solve(AF))
+      Z <- eigen(W)$values
+      pval <- pwchisq(statistic, Z, lower.tail = FALSE)
+    }
+    else{
+      names(statistic) <- "chisq"
+      pval <- pchisq(statistic, parameter, lower.tail = FALSE)
+    }
+  }
+  if (type == "overlapping"){
+    BFG <- t(gradx)%*%grady/n
+    BGF <- t(grady)%*%gradx/n
+    W <- rbind(cbind(-BF%*%solve(AF),-BFG%*%solve(AG)),
+               cbind(-BGF%*%solve(AF),-BG%*%solve(AG))
+               )
+    method <- "Vuong Test (overlapping)"
+    if (! hyp){
+      statistic <- c(wchisq = n*w2)
+      Z <- eigen(W)$values^2
+      pval <- pwchisq(statistic, Z, lower.tail = FALSE)
+      alternative <- c("two different models : use non-nested version of the Vuong test")
+      parameter <- c(df = length(Z))
+    }
+    else{
+      statistic <- c(wchisq = 2*LR)
+      Z <- eigen(W)$values
+      print(Z)
+      q1 <- qwchisq(0.95,Z)
+      q2 <- qwchisq(0.05,Z)
+      pval <- pwchisq(statistic, Z)
+      alternative <- c("je sais pas")
+      parameter <- c(df = length(Z))
+    }
+  }
+  result <- list(statistic = statistic,
+                 method = method,
+                 p.value = pval,
+                 data.name = data.name,
+                 alternative = alternative,
+                 parameter = parameter)
+  class(result) <- "htest"
+  result
+}
+
